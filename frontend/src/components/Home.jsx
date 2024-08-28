d6ef9b5ee3f68b2187e2420b54ef993cde545c50
Mukul <mukulraj.21ug2005@iiitranchi.ac.in>
2024-08-28 16:27:07 +0530

    home page comleted

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
 import CategoryCarousel from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
+import Footer from './shared/Footer'
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <HeroSection/>
       <CategoryCarousel/>
       <LatestJobs/>
+      <Footer/>
     </div>
   )
 }
